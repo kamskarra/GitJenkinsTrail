0f7dfe43fe4784a19133cd63b335b36543e94bda
kamskarra <66778939+kamskarra@users.noreply.github.com>
2020-07-02 18:10:00 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 	
 	public static void main(String args[]) {
-		System.out.println("Hello world kamesh try 6");
+		System.out.println("Hello world kamesh try 7");
 	}
 
 }
